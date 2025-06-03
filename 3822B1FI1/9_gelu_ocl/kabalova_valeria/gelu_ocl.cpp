fc7cb9483ece8075020adf2714a6f95029fad246
m1likus <m1likus@yandex.ru>
2025-06-03 20:14:31 +0300

    another day, another fix

--- 3822B1FI1/9_gelu_ocl/kabalova_valeria/gelu_ocl.cpp
+++ 3822B1FI1/9_gelu_ocl/kabalova_valeria/gelu_ocl.cpp
@@ -2,30 +2,30 @@
 #include <CL/cl.h>
 #include <cstring>
 
-const char* source =
-"__kernel void kernel(__global const float* input, __global float* result, const int n) { \n"\
-" int id = get_global_id(0);\n"\
-" if (id < n) {\n"\
-"   float x = input[id];\n"\
-"   float arg1 = 0.79788458347320556640625f * (x + 0.044715f * x * x * x);\n"\
-"   float tmp;\n"\
-"   if (x <= 0.0f) tmp = tanh(arg1);\n"\
-"   else if (arg1 >= 4.1f) tmp = 1.0f;\n"\
-"   else {\n"\
-"     if (x < 0.0001f) tmp = x;\n"\
-"     else {\n"\
-"     float x_2 = x * x;\n"\
-"     tmp = x * (10395.0f + x_2 * (1260.0f + x_2 * 21.0f));\n"\
-"     tmp = tmp / (10395.0f + x_2 * (4725.0f + x_2 * (210.0f + x_2)));\n"\
-"     }\n"\
-"   }\n"\
-"  result[id] = 0.5f * x * (1.0f + tmp);\n"\
-"  }\n"\
-"}\n";
+const char* source = R"(
+  __kernel void kernel(__global const float* input, __global float* result, const int n) { 
+    int id = get_global_id(0);
+    if (id < n) {
+    float x = input[id];
+    float arg1 = 0.79788458347320556640625f * (x + 0.044715f * x * x * x);
+    float tmp;
+    if (x <= 0.0f) tmp = tanh(arg1);
+    else if(x < 0.0001f) tmp = x;
+    else if (arg1 >= 4.1f) tmp = 1.0f;
+    else {
+      float x_2 = x * x;
+      tmp = x * (10395.0f + x_2 * (1260.0f + x_2 * 21.0f));
+      tmp = tmp / (10395.0f + x_2 * (4725.0f + x_2 * (210.0f + x_2)));
+    }
+    result[id] = 0.5f * x * (1.0f + tmp);
+    }
+  }
+)";
 
 std::vector<float> GeluOCL(const std::vector<float>& input) {
-  std::vector<float> result(input.size());
-
+  const size_t size = input.size();
+  std::vector<float> result(size);
+  
   cl_platform_id platform;
   clGetPlatformIDs(1, &platform, nullptr);
 
@@ -33,32 +33,27 @@ std::vector<float> GeluOCL(const std::vector<float>& input) {
   clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, nullptr);
 
   cl_context context;
-  context = clCreateContextFromType(nullptr, CL_DEVICE_TYPE_GPU, nullptr, nullptr, nullptr);
+  context = clCreateContext(nullptr, 1, &device, nullptr, nullptr, nullptr);
 
   cl_command_queue queue;
-  cl_queue_properties properties[] = {CL_QUEUE_PROPERTIES, 0, 0};
-  queue = clCreateCommandQueueWithProperties(context, device, properties, nullptr);
+  queue = clCreateCommandQueue(context, device, 0, nullptr);
 
   cl_mem in, out;
-  in = clCreateBuffer(context, CL_MEM_READ_ONLY, input.size() * sizeof(float), nullptr, nullptr);
-  out = clCreateBuffer(context, CL_MEM_WRITE_ONLY, input.size() * sizeof(float), nullptr, nullptr);
+  in = clCreateBuffer(context, CL_MEM_READ_ONLY, size * sizeof(float), nullptr, nullptr);
+  out = clCreateBuffer(context, CL_MEM_WRITE_ONLY, size * sizeof(float), nullptr, nullptr);
+  clEnqueueWriteBuffer(queue, in, CL_TRUE, 0, size * sizeof(float), input.data(), 0, nullptr, nullptr);
 
-  clEnqueueWriteBuffer(queue, in, CL_TRUE, 0, input.size() * sizeof(float), input.data(), 0, nullptr, nullptr);
-
-  size_t srclen[] = { strlen(source) };
-  cl_program program = clCreateProgramWithSource(context, 1, &source, srclen, nullptr);
+  cl_program program = clCreateProgramWithSource(context, 1, &source, nullptr, nullptr);
   clBuildProgram(program, 1, &device, nullptr, nullptr, nullptr);
   cl_kernel kernel = clCreateKernel(program, "kernel", nullptr);
-
-  const size_t size = input.size();
 
   clSetKernelArg(kernel, 0, sizeof(cl_mem), &in);
   clSetKernelArg(kernel, 1, sizeof(cl_mem), &out);
   clSetKernelArg(kernel, 2, sizeof(int), &size);
 
-  size_t group;
-  clGetKernelWorkGroupInfo(kernel, device, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, sizeof(size_t), &group, nullptr);
-  clEnqueueNDRangeKernel(queue, kernel, 1, nullptr, &size, &group, 0, nullptr, nullptr);
+  size_t localSize = 256;
+  size_t globalSize = (size + localSize - 1) / localSize * localSize;
+  clEnqueueNDRangeKernel(queue, kernel, 1, nullptr, &globalSize, &localSize, 0, nullptr, nullptr);
   clFinish(queue);
 
   clEnqueueReadBuffer(queue, out, CL_TRUE, 0, size * sizeof(float), result.data(), 0, nullptr, nullptr);
